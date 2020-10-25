74f533eeacaee9ee92190289b89c97950aec83ff
Morgan Reilly <morgan.reilly27@gmail.com>
2020-10-25 22:58:22 +0000

    Final Submission Upload

--- numint.py
+++ numint.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
 """NUI Galway CT5132/CT5148 Programming and Tools for AI (James McDermott)
 
 Skeleton/solution for Assignment 1: Numerical Integration
@@ -14,9 +20,7 @@ Student ID(s): 20230091, 20235398, 20231242
 import numpy as np
 import sympy
 import itertools
-import decimal
 import math
-
 
 def numint_py(f, a, b, n):
     """Numerical integration. For a function f, calculate the definite
@@ -31,23 +35,23 @@ def numint_py(f, a, b, n):
     4.64746
 
     """
-
     A = 0
-    w = (b - a) / n  # width of one slice
+    w = (b - a) / n # width of one slice
+    
+    import decimal
 
-    def f_input(x1, x2, width):
-        while x1 < x2:
-            yield float(x1)
-            x1 += decimal.Decimal(w)
+    def Finput(a, b, w): # function to get list of input points to be passed to the function 
+        while a < b: 
+            yield float(a)
+            a += decimal.Decimal(w)
+            
+    x = list(Finput(a, b, str(w))) # create input list
 
-    x = list(f_input(a, b, str(w)))
-
+    
     for i in x:
-        y = f(i)
-        A += y * w
-
+        A += f(i) * w # # using lb scheme: calculate f(x) * dx for each value in input list and increment A 
+        
     return A
-
 
 def numint(f, a, b, n, scheme='mp'):
     """Numerical integration. For a function f, calculate the definite
@@ -67,30 +71,22 @@ def numint(f, a, b, n, scheme='mp'):
     4.69417
 
     """
-
     # STUDENTS ADD CODE FROM HERE TO END OF FUNCTION
     A = 0
-    w = (b - a) / n  # width of one slice
-    x = np.arange(a, b, w)
-
-    l = str(w)
-    d = str.split(l, '.')
-    dl = len(d[1])
+    w = (b - a) / n # width of one slice
+    x = np.arange(a,b,w) # create list of input points to be passed to the function 
 
     for i in x:
-        if scheme == 'mp':
-            yl = np.round(f(i), dl)
-            yr = np.round(f(i + w), dl)
-            A += ((yl + yr) / 2) * w
+        if scheme == 'mp':            
+            A += f((i + (i + w)) / 2) * w # using mp scheme: calculate f((x + (x+1) / 2)) * dx for each value in input list and increment A
+            
         elif scheme == 'lb':
-            y = f(i)
-            A += y * w
+            A += f(i) * w # using lb scheme: calculate f(x) * dx for each value in input list and increment A 
+            
         elif scheme == 'ub':
-            y = f(i + w)
-            A += y * w
-
-    return A
-
+            A += f(i + w) * w # using ub scheme: calculate f(x) * dx for each value in input list and increment A 
+            
+    return A    
 
 def true_integral(fstr, a, b):
     """Using Sympy, calculate the definite integral of f from a to b and
@@ -111,11 +107,10 @@ def true_integral(fstr, a, b):
     """
     x = sympy.symbols("x")
     # make fsym, a Sympy expression in x, now using eg "sympy.sin"
-    fsym = eval(fstr.replace("np", "sympy"))
-    A = sympy.integrate(fsym, (x, a, b))  # definite integral
-    A = float(A.evalf())  # convert to float
+    fsym = eval(fstr.replace("np", "sympy")) 
+    A = sympy.integrate(fsym, (x, a, b)) # definite integral
+    A = float(A.evalf()) # convert to float
     return A
-
 
 def numint_err(fstr, a, b, n, scheme):
     """For a given function fstr and bounds a, b, evaluate the error
@@ -131,16 +126,15 @@ def numint_err(fstr, a, b, n, scheme):
     0.3333 0.0483 0.1450
 
     """
-    f = eval("lambda x: " + fstr)  # f is a Python function
+    f = eval("lambda x: " + fstr) # f is a Python function
     A = true_integral(fstr, a, b)
     # STUDENTS ADD CODE FROM HERE TO END OF FUNCTION
-
+    
     T = numint(f, a, b, n, scheme)
     AE = abs(A - T)
     RE = abs(AE / A)
-
-    return A, AE, RE
-
+    
+    return (A, AE, RE)
 
 def make_table(f_ab_s, ns, schemes):
     """For each function f with associated bounds (a, b), and each value
@@ -165,34 +159,28 @@ def make_table(f_ab_s, ns, schemes):
     np.sin(x),0.00,1.00,100,lb,0.4597,0.004211,0.009161
     np.sin(x),0.00,1.00,100,mp,0.4597,1.915e-06,4.167e-06
 
-    """
-    funcs = []
-    # Use list comprehension to unzip list of tuples
-    for i in list(zip(*f_ab_s)):
-        funcs.append(i)
-
-    numpy_funcs = np.array(funcs)
-    transposed_funcs = numpy_funcs.T
-
-    transposed_funcs_list = transposed_funcs.tolist()
-
-    for i in transposed_funcs_list:
-        vals = numint_err(i[0], int(i[2]), ns, schemes)
-        print(vals)
-
+    """   
+    # STUDENTS ADD CODE FROM HERE TO END OF FUNCTION
     for f_ab_s, ns, schemes in itertools.product(f_ab_s, ns, schemes):
-        print(f_ab_s, ns, schemes, numint_err(f_ab_s, ns, schemes))
-
-
+        res = numint_err(f_ab_s[0], f_ab_s[1], f_ab_s[2], ns, schemes)
+        print("%s,%.2f,%.2f,%d,%s,%.4g,%.4g,%.4g" % (f_ab_s[0], f_ab_s[1], f_ab_s[2], ns, schemes, res[0], res[1], res[2]))
+    
 def main():
     """Call make_table() as specified in the pdf."""
     # STUDENTS ADD CODE FROM HERE TO END OF FUNCTION
-
+    make_table([("np.cos(x)", 0, math.pi/2), ("np.sin(2*x)", 0, 1), ("np.exp(x)", 0, 1)],  [10, 100, 1000], ['lb', 'mp'])
+    
+    # uncomment to run the numint_nd function
+    # print(round(numint_nd([np.sin, np.cos],[0, 1],[1, 2],[100, 100]), 5))
+    # print(round(numint_nd([np.exp, np.exp, np.exp],[0, 1, 0],[1, 2, 3],[1000, 1000, 1000]), 5))
 
 """
-STUDENTS REPLACE THIS TEXT WITH INTERPRETATION OF main() RESULTS
+On comparing the results of each function with the 'lb' and 'mp' schemes, 'mp' scheme has better results than 'lb' scheme. 
+We can also infer that as the number of slices 'n' increases, the Relative error decreases for the same scheme.
+For example, in the function, "sin(2x)" when 'n = 10', the difference between the relative error of the 'lb' and 'mp' scheme 
+is 0.06587. Whereas, when 'n = 1000', the difference between the relative error of the 'lb' and 'mp' scheme is 0.000642.
+Therefore, the results of the 'mp' scheme with higher value of 'n' is closest to the value of the True Integral. 
 """
-
 
 def numint_nd(f, a, b, n):
     """numint in any number of dimensions.
@@ -203,20 +191,44 @@ def numint_nd(f, a, b, n):
     n: a tuple of m values indicating the number of steps per dimension
 
     STUDENTS ADD DOCTESTS
+    >>> round(numint_nd([np.sin, np.cos],[0, 1],[1, 2],[100, 100]), 5)
+    0.03113
+    
+    >>> round(numint_nd([np.exp, np.exp, np.exp],[0, 1, 0],[1, 2, 3],[1000, 1000, 1000]), 5)
+    153.30086
 
     """
 
     # My implementation uses Numpy and the mid-point scheme, but you
     # are free to use pure Python and/or any other scheme if you prefer.
-
+    
     # Hint: calculate w, the step-size, per dimension
     w = [(bi - ai) / ni for (ai, bi, ni) in zip(a, b, n)]
 
     # STUDENTS ADD CODE FROM HERE TO END OF FUNCTION
+    # create a list of input points for each dimension
+    points = [np.linspace(ai, bi, ni) for (ai, bi, ni) in zip(a, b, n)]
+
+    integrals = []
+
+    for p, f, w in zip(points, f, w):
+        R = 0
+        for x in p:        
+            R += (f(x)) * w # Calculate the f(x) * dx for each dimension
+        integrals.append(R) # List of all f(x) * dx values
+
+    Final = np.prod(integrals) # Product of all f(x) * dx values. Eg: For double integral: f(x) * dx * f(y) * dy
+    return Final
 
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
     main()
+
+
+# In[ ]:
+
+
+
+
